f7dee6c2ab01eaefe2145f23ed3709fd1afe4f54
Trenton K <trentonk@Trentons-MacBook-Pro-2.local>
2022-08-21 13:38:40 -0500

    build static html

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,8 @@
 import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
 import { defineConfig } from "astro/config";
-import vercelEdge from '@astrojs/vercel/static';
 
 // https://astro.build/config
 export default defineConfig({
-  output: 'server',
-  adapter: vercelEdge(),
   integrations: [tailwind(), react()],
 });
